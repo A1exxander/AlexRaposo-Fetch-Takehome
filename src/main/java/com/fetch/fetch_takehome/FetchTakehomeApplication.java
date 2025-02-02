b73a12bc773892f17b11de9a35ac0a6ad7979b26
Alex <raposoalexander@gmail.com>
2025-02-02 00:44:20 -0500

    Initialized project

---
+++ src/main/java/com/fetch/fetch_takehome/FetchTakehomeApplication.java
@@ -0,0 +1,13 @@
+package com.fetch.fetch_takehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchTakehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchTakehomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fetch/fetch_takehome/FetchTakehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch.fetch_takehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchTakehomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
